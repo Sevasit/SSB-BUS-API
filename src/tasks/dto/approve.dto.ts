44897b6669717f7f4af40520039354cd3469258b
Sevasit <new12253952@gmail.com>
2024-02-14 23:37:30 +0700

    add history page

--- src/tasks/dto/approve.dto.ts
+++ src/tasks/dto/approve.dto.ts
@@ -1,10 +1,8 @@
 import { IsDate, IsString } from 'class-validator';
 
-export class RejectTaskDto {
+export class ApproveTaskDto {
   @IsString()
   readonly id: string;
   @IsString()
   readonly processBy: string;
-  @IsString()
-  readonly annotation: string;
 }
